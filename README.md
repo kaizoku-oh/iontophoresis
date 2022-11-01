1f3d2792c47dabfa3905f748a264f8d9f392947e
kaizoku-oh <garssallaoui.bayrem@gmail.com>
2022-11-02 00:33:05 +0100

    Added motor driver control
    Added motor driver control

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # iontophoresis
 A device that helps people with hyperhidrosis conditions
+
+# TODO:
+
+[] Verify that the code is working as expected by measuring the output voltage with multimeter
+
+[] Add EEPROM storage for tracking session numbers
+
+[] Add a serial command interface to start/pause/stop session, change voltage output, reading EEPROM data
+
+[] Add a GUI on top of the serial command interface
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,38 @@
 #include <Arduino.h>
 
+const int IN1 = 7;
+const int IN2 = 6;
+const int IN3 = 5;
+const int IN4 = 4;
+const int ENA = 10;
+const int ENB = 11;
+const int TIME_TO_WAIT_BEFORE_STARTING = 20;
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(115200);
+  for(int i = 0; i < TIME_TO_WAIT_BEFORE_STARTING; i++) {
+    Serial.print("Starting session in ");
+    Serial.print(TIME_TO_WAIT_BEFORE_STARTING - i);
+    Serial.println(" seconds...");
+    delay(1000);
+  }
+  Serial.println("Session started");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
-  Serial.println("Running in loop()...");
+  //control speed 
+  analogWrite(ENA, 255);
+  analogWrite(ENB, 255); 
+  //control direction 
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
 }
